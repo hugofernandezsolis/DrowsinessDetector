6a1fbd2b291b11a91b103972aab4719c14cfc8ee
Hugo Fernández Solís <hugofernandezsolis@gmail.com>
2022-07-12 18:37:31 +0100

    Repository inisialization

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/**
+ * @author Hugo Fernández Solís.
+ * @date 06/07/2022
+ * @file main.cpp
+ * 
+ * @brief COntains the main function for the DriverDrowsinessDetector system. This function checks all the parameters
+ * passed to the programs and executes the corresponding tasks.
+ */
+
+ #include <iostream>
+
+ int main(int arg, char* argv[]) {
+  std::cout << "Hello World!" << std::endl;
+  return EXIT_SUCCESS;
+ }
